e4ae75a98268ec36fe0c87d8d6c64851edea64e4
unknown <vskreddy652@yahoo.com>
2020-03-12 13:10:53 +0530

    first commit

---
+++ src/main/java/com/myabcd/App.java
@@ -0,0 +1,13 @@
+package com.myabcd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World11111111111111!" );
+    }
+}
---
+++ src/test/java/com/myabcd/AppTest1.java
@@ -0,0 +1,29 @@
+package com.myabcd;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest1 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+		System.out.println("in test caseeeeeeeeeeeee");
+        assertFalse( true );
+    }
+	
+	@Test
+    public void shouldAnswerWithTrue1()
+    {
+		System.out.println("in test case2222222222222");
+        assertTrue( true );
+    }
+}
